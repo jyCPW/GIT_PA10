3a56b9ac1990687860bbd412a72b9aecbd651b14
jyCPW <64114949+jyCPW@users.noreply.github.com>
2020-06-18 11:57:10 +0800

    PA10

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,20 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
+    //3.55,-3.7
     private Animation thisAnimation;
+    private Rigidbody thisrigidbody;
+    public float forceY = 0;
+    public Text Scoretxt;
+    public float score;
 
     void Start()
     {
         thisAnimation = GetComponent<Animation>();
         thisAnimation["Flap_Legacy"].speed = 3;
+        thisrigidbody = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
+        float ClampY = Mathf.Clamp(transform.position.y, -3.7f, 3.55f);
+        transform.position = new Vector3(0, ClampY, 0);
         if (Input.GetKeyDown(KeyCode.Space))
+        {
+            thisrigidbody.AddForce(transform.up * forceY);
             thisAnimation.Play();
+        }
+            
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "obstacle")
+        {
+            SceneManager.LoadScene("GameoverScene");
+        }
+        if(other.tag == "ground")
+        {
+            SceneManager.LoadScene("GameoverScene");
+        }
+
+        if(other.gameObject.tag == "Score")
+        {
+            score++;
+            Scoretxt.text = "Score : " + score;
+        }
     }
 }
---
+++ Assets/Scripts/Restart.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Restart : MonoBehaviour
+{
+    public Button Restartbtn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button RTbtn = Restartbtn.GetComponent<Button>();
+        RTbtn.onClick.AddListener(RTbtnOnclick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void RTbtnOnclick()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
